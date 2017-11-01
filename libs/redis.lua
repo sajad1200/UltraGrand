2a214565924607f9aa810271c8eba12397857d0f
sajad1200 <sego.sego1122@gmail.com>
2017-11-01 05:34:25 +0300

    UltraGrand

--- libs/redis.lua
+++ libs/redis.lua
@@ -1,5 +1,5 @@
 local Redis = (loadfile "./libs/lua-redis.lua")()
---local FakeRedis = (loadfile "./tg/max/fakeredis.lua")()
+local FakeRedis = (loadfile "./libs/fakeredis.lua")()
 
 local params = {
   host = '127.0.0.1',
